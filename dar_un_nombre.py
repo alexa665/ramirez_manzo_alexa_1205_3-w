7ca0d1eae0fbe99bf42ef6dccc2d2f86deb1fc70
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-10-09 20:36:07 -0600

    Add files via upload

---
+++ dar_un_nombre.py
@@ -0,0 +1,6 @@
+print(" ")
+print("alexa guadalupe ramirez manzo 1205 3-W")
+print(" ")
+#Dar un string <nombre> y despliegue un saludo ¡hola <nombre>!.
+my_funcion = "alexa guadalupe ramirez manzo"#funsion para el nombre
+print("¡hola " + my_funcion + "!") # saludo
